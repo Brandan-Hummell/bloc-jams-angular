e85ad704fa7f1c211f1b5157c56d58c8fcbb24c7
Brandan Hummell <bh2df@virginia.edu>
2017-07-12 23:40:28 -0400

    Checkpoint 5: Controllers for landing page and collection page

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,45 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 196, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 263, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 223, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 256, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 229, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+ var albumBobs = {
+     title: "Black on Both Sides",
+     artist: "Mos Def",
+     label: "Rawkus Records",
+     year: 1999,
+     albumArtUrl: "assets/images/album_covers/Bobs.jpg",
+     songs: [
+         { title: 'Fear Not of Man', duration: '4:30' },
+         { title: 'Speed Law', duration: '4:16' },
+         { title: 'New World Water', duration: '3:12' },
+         { title: 'Love', duration: '4:23' },
+         { title: 'Mathematics', duration: '4:06' }
+     ]
+ };
